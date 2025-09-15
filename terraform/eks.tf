2ef32a8137a46fe09515f1b38a8db71dead162ce
Joao Pedro <joaopedrot1912@gmail.com>
2025-09-15 15:16:35 -0300

    fix: addons

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,12 +1,12 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 20.0.0"
+  version = "~> 21.0"
 
   kubernetes_version = "1.33"
 
   name = "${var.name_prefix}-cluster"
 
-  endpoint_public_access = true
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
   vpc_id = module.vpc.vpc_id
@@ -16,38 +16,28 @@ module "eks" {
     Project     = "eks-lab"
   }
 
-  subnet_ids   = module.vpc.private_subnets
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 1
       desired_size = 1
     }
-
   }
 
   addons = {
-    vpc_cni = {
-      addon_name        = "vpc-cni"
-      addon_version     = "v1.17.2-eksbuild.1"
-      resolve_conflicts = "OVERWRITE"
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
     }
-    kube_proxy = {
-      addon_name        = "kube-proxy"
-      addon_version     = "v1.26.2-eksbuild.1"
-      resolve_conflicts = "OVERWRITE"
-    }
-    coredns = {
-      addon_name        = "coredns"
-      addon_version     = "v1.12.0-eksbuild.1"
-      resolve_conflicts = "OVERWRITE"
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
     }
   }
-
 }
